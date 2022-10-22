9a78663e95e13677e217ea2137388fc50ba09005
Anton Golubeykov <ds6580@yandex.ru>
2022-10-22 23:16:43 +0300

    Added image loader

---
+++ Fake Store/Services/Networking/ImageLoader/ImageLoader.swift
@@ -0,0 +1,33 @@
+//
+//  ImageLoader.swift
+//  Fake Store
+//
+//  Created by Антон Голубейков on 22.10.2022.
+//
+
+import UIKit
+
+struct ImageLoader {
+
+    let queueForLoad = DispatchQueue.global(qos: .utility)
+    let session = URLSession(configuration: .default)
+
+    func loadImage(from url: URL, _ onLoadWasCompleted: @escaping (_ result: Result<UIImage, Error>) -> Void) {
+        let imageCache = NSCache<AnyObject, AnyObject>()
+
+        if let imageFromCache = imageCache.object(forKey: url as AnyObject) as? UIImage {
+            onLoadWasCompleted(.success(imageFromCache))
+        } else {
+        session.dataTask(with: url) { data, _, error in
+            if let error = error {
+                onLoadWasCompleted(.failure(error))
+            }
+            if let data = data, let image = UIImage(data: data) {
+                imageCache.setObject(image, forKey: url as AnyObject)
+                onLoadWasCompleted(.success(image))
+            }
+        }
+        .resume()
+    }
+    }
+}
